4e1c1b391e19d08cec139c5b5ea2df434103d71f
reneehu <rhu@applevac.com>
2014-07-13 16:56:57 -0400

    <message>

--- src/samples/webflow/Cart.java
+++ src/samples/webflow/Cart.java
@@ -7,7 +7,7 @@ import java.util.List;
 import java.util.Map;
 
 public class Cart implements Serializable{
-	private static final long serialVersionUID = 7901330827203016310L; 
+	private static final long serialVersionUID = 79013308L; 
 	private Map<Integer, CartItem> map = new HashMap<Integer,CartItem>();
 	
 	public List<CartItem> getItems(){
